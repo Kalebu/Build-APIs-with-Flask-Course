f3c7e2c9050715525a44c42f66be5c7c0ae28d63
Kalebu <isaackeinstein@gmail.com>
2021-04-23 22:29:06 +0300

    updated procfile

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                     'response': "You're signature expired , login for another token"
                 }
 
-    app.run(debug=True)
+    # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     
