e1aafd3a3265f5f4b6ee3abbf1cebc7f77b9bc0c
Kalebu <isaackeinstein@gmail.com>
2021-04-23 22:27:39 +0300

    updated procfile

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import request
 from main import create_app
 from main.users import serializer
@@ -28,3 +29,6 @@ if __name__ == '__main__':
                 }
 
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
